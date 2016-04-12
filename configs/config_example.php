5c9c78615268f5175741587db3333f9e107bc36f
Greg Arnold <greg@arnoldassociates.com>
2016-04-12 14:39:46 -0400

    Handle crypt() as the password.

--- configs/config_example.php
+++ configs/config_example.php
@@ -43,6 +43,7 @@ $cfg['default_uid'] = ""; //if empty next incremental will be default
 $cfg['default_homedir'] = "/srv/ftp";
 // Use either SHA1 or MD5 or any other supported by your MySQL-Server and ProFTPd
 // "pbkdf2" is supported if you are using ProFTPd 1.3.5.
+// "crypt" uses the unix crypt() function.
 $cfg['passwd_encryption'] = "SHA1";
 $cfg['min_passwd_length'] = "6";
 $cfg['max_userid_length'] = "20";
