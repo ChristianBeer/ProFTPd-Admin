945c7370ed7d80fa99db3aeb8b8c9526ff2eaadd
Greg Arnold <greg@arnoldassociates.com>
2016-04-19 13:18:26 -0400

    Fix license and bracing.

--- includes/unix_crypt.php
+++ includes/unix_crypt.php
@@ -1,29 +1,27 @@
 <?php
 /**
- * @author Greg Arnold
+ * @author    Greg Arnold
+ * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
+ * @copyright Grag Arnold <greg@arnoldassociates.com>
  */
 
-function unix_crypt($password)
-  {
+function unix_crypt($password) {
   global $cfg;
   $chars = './0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   $encrypt_method = is_array($cfg) && array_key_exists('encrypt_method', $cfg) ? $cfg['encrypt_method'] : "MD5";
   $salt = "";
 
-  if ($cfg['read_login_defs'])
-    {
+  if ($cfg['read_login_defs']) {
     $login_defs = file_get_contents("/etc/login.defs");
-    if ($login_defs !== -1)
-      {
+    if ($login_defs !== -1) {
       if (preg_match('/[\r\n]+\s*ENCRYPT_METHOD\s+(.*?)[\r\n]/', $login_defs, $matches))
         $encrypt_method = $matches[1];
 
       /* XXX: Read number of rounds from login.defs */
-      }
     }
+  }
 
-  switch ($encrypt_method)
-    {    
+  switch ($encrypt_method) {    
     case "MD5":
       $salt = '$1$';
       for ($i = 0; $i < 8; $i++)
@@ -48,7 +46,7 @@ function unix_crypt($password)
       for ($i = 0; $i < 2; $i++)
         $salt .= $chars[mt_rand(0, strlen($chars) - 1)];
       break;
-    }
+  }
 
   return crypt($password, $salt);
-  }
+}
