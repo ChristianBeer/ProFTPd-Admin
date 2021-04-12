46ac8fc5cec3430737a2a1a59b0c74fabf1166b7
preussal <reg@mailjunkie.de>
2021-04-12 15:23:36 +0700

    update to CRLF
    update to CRLF

--- install/tables.sql
+++ install/tables.sql
@@ -29,10 +29,15 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL default '',
   `title` varchar(5) NOT NULL default '',
   `company` varchar(255) NOT NULL default '',
+  `quota_type` enum('user','group','class','all') NOT NULL default 'user',
+  `per_session` enum('false','true') NOT NULL default 'false',
+  `limit_type` enum('soft','hard') NOT NULL default 'soft',
   `bytes_in_used` bigint(20) unsigned NOT NULL default '0',
   `bytes_out_used` bigint(20) unsigned NOT NULL default '0',
+  `bytes_xfer_avail` int(10) unsigned NOT NULL default '0',
   `files_in_used` bigint(20) unsigned NOT NULL default '0',
   `files_out_used` bigint(20) unsigned NOT NULL default '0',
+  `files_xfer_avail` int(10) unsigned NOT NULL default '0'
   `login_count` int(11) unsigned NOT NULL default '0',
   `last_login` datetime NOT NULL default '0000-00-00 00:00:00',
   `last_modified` datetime NOT NULL default '0000-00-00 00:00:00',
@@ -40,3 +45,15 @@ CREATE TABLE `users` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `userid` (`userid`)
 ) ENGINE=MyISAM COMMENT='ProFTPd user table';
+
+
+CREATE TABLE quotatallies (
+  `name` varchar(30) NOT NULL default '',
+  `quota_type` enum('user','group','class','all') NOT NULL default 'user',
+  `bytes_in_used` int(10) unsigned NOT NULL default '0',
+  `bytes_out_used` int(10) unsigned NOT NULL default '0',
+  `bytes_xfer_used` int(10) unsigned NOT NULL default '0',
+  `files_in_used` int(10) unsigned NOT NULL default '0',
+  `files_out_used` int(10) unsigned NOT NULL default '0',
+  `files_xfer_used` int(10) unsigned NOT NULL default '0'
+) ENGINE=MyISAM COMMENT='ProFTPd Quota table';
